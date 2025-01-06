a62729c54b55d767ac1dcfa66a928933b8244ad8
sohaib440 <sohaibmushtaq5@gmail.com>
2025-01-06 18:19:49 +0500

    Partial

--- routes/admin/lead.route.js
+++ routes/admin/lead.route.js
@@ -5,50 +5,49 @@ const upload = require("../../config/multer");
 const passport = require("../../middleware/passportAuth.middleware");
 const middleware = require("../../middleware/index.middleware");
 
-//
-router.get(
-  "/view-lead",
-  passport.authenticate("jwt", { session: false }),
-  middleware.checkPermission("read"), 
-  (req, res) => {
-    res.send("Leads Data");
-  }
-);
+// //
+// router.get(
+//   "/view-lead",
+//   passport.authenticate("jwt", { session: false }),
+//   middleware.checkPermission("read"), 
+//   (req, res) => {
+//     res.send("Leads Data");
+//   }
+// );
 
 router.post(
   "/create-lead",
   passport.authenticate("jwt", { session: false }),
-  middleware.adminRoleCheck,
+  middleware.checkPermission("create"), 
   controller.adminController.lead.createLead
 );
 
 router.get(
   "/all-leads",
   passport.authenticate("jwt", { session: false }),
-  middleware.adminRoleCheck,
+  middleware.checkPermission("read"), 
   controller.adminController.lead.getAllLeads
 );
 
 router.get(
   '/:leadId',
   passport.authenticate('jwt', { session: false }),
-  middleware.adminRoleCheck,
+  middleware.checkPermission("read"), 
   controller.adminController.lead.getLeadById
 )
 
 router.patch(
   "/approve/:leadId",
   passport.authenticate("jwt", { session: false }),
-  middleware.adminRoleCheck,
+  middleware.checkPermission("update"), 
   controller.adminController.lead.approveLeadById
 );
 
 router.delete(
   "/delete/:leadId",
   passport.authenticate("jwt", { session: false }),
-  middleware.adminRoleCheck,
+  middleware.checkPermission("delete"), 
   controller.adminController.lead.deleteLead,
 );
-
 
 module.exports = router;
